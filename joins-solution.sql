3712ea7019b9622f354982b9b03056982f33fb1e
Kevin Anderson <kmichael419@hotmail.com>
2020-07-28 11:46:15 -0500

    question 2 complete

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,9 +1,12 @@
 --1. Get all customers and their addresses.
-
+SELECT * FROM "customers"
+JOIN "addresses" ON "addresses"."customer_id" = "customers"."id";
 
 
 --2.Get all orders and their line items (orders, quantity and product).
-
+SELECT * FROM "orders"
+JOIN "line_items" ON "line_items"."order_id" = "orders"."id"
+JOIN "products" ON "products"."id" = "line_items"."product_id";
 
 
 --3.Which warehouses have cheetos?
